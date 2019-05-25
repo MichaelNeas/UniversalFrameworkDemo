d00e2ea69ad06506cdbf72314e8aa40ddeaa1606
michaelneas <michael.r.neas@gmail.com>
2019-05-25 18:53:13 -0400

    demo showing linked universal framwork in an application

---
+++ Test/Test/Test.h
@@ -0,0 +1,19 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by Michael Neas on 5/25/19.
+//  Copyright © 2019 Michael Neas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Test.
+FOUNDATION_EXPORT double TestVersionNumber;
+
+//! Project version string for Test.
+FOUNDATION_EXPORT const unsigned char TestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test/PublicHeader.h>
+
+
---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Michael Neas on 5/25/19.
+//  Copyright © 2019 Michael Neas. All rights reserved.
+//
+
+import UIKit
+import Test
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(Test.TestVersionNumber)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
